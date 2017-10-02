e5887467dad698eca4590d84ef570a9fe28fc200
matthew <m5w@users.noreply.github.com>
2017-10-02 14:27:36 -0400

    Use an array of structs to represent all the test cases

--- tests/test_lerp_cell_voltage.cc
+++ tests/test_lerp_cell_voltage.cc
@@ -8,9 +8,16 @@
 #include "floating_point_type.h"
 #include "lerp_cell_voltage.h"
 
+typedef struct CheckLerpCellVoltageBufType {
+  const FloatingPointType charge_discharged_from_cell;
+  const FloatingPointType cell_voltage_source_voltage;
+  const ErrorType the_error;
+} CheckLerpCellVoltageBufType;
+
 static inline void check_lerp_cell_voltage(
-    const FloatingPointType charge, const ErrorType the_error,
-    const FloatingPointType the_voltage,
+    const FloatingPointType charge_discharged_from_cell,
+    const FloatingPointType cell_voltage_source_voltage,
+    const ErrorType the_error,
     LerpCellVoltageLinearBufType *const linear_buf_pointer,
     LerpCellVoltageBinaryBufType *const binary_buf_pointer);
 
@@ -21,8 +28,6 @@ BOOST_AUTO_TEST_CASE(test_lerp_cell_voltage) {
       {0.0f, 1.0f}, {1.0f, 2.0f}, {2.0f, 4.0f}};
   constexpr std::size_t number_of_points =
       sizeof(points) / sizeof(CellDischargeCurvePoint);
-  FloatingPointType linear_voltage;
-  FloatingPointType binary_voltage;
 
   LerpCellVoltageLinearBufType linear_buf =
       lerp_cell_voltage_linear_buf_type(points, number_of_points);
@@ -31,47 +36,45 @@ BOOST_AUTO_TEST_CASE(test_lerp_cell_voltage) {
       lerp_cell_voltage_binary_buf_type(points, number_of_points);
   BOOST_CHECK(error == NONE);
 
-#define CHECK_LERP_CELL_VOLTAGE(the_error, charge, the_voltage)               \
-  check_lerp_cell_voltage((charge), (the_error), (the_voltage), &linear_buf,  \
-                          &binary_buf)
+  const CheckLerpCellVoltageBufType bufs[] = {
+      {-2.00f, ERROR_FLOATING_POINT_TYPE, EXTRAPOLATION_BELOW},
+      {-1.00f, ERROR_FLOATING_POINT_TYPE, EXTRAPOLATION_BELOW},
+      {+0.00f, +1.00f, NONE},
+      {+0.00f, +1.00f, NONE},
+      {+0.50f, +1.50f, NONE},
+      {+0.75f, +1.75f, NONE},
+      {+1.00f, +2.00f, NONE},
+      {+1.00f, +2.00f, NONE},
+      {+1.50f, +3.00f, NONE},
+      {+1.75f, +3.50f, NONE},
+      {+2.00f, +4.00f, NONE},
+      {+2.00f, +4.00f, NONE},
+      {+3.00f, ERROR_FLOATING_POINT_TYPE, EXTRAPOLATION_ABOVE},
+      {+4.00F, ERROR_FLOATING_POINT_TYPE, EXTRAPOLATION_ABOVE}};
+  const CheckLerpCellVoltageBufType *const bufs_end =
+      bufs + sizeof(bufs) / sizeof(CheckLerpCellVoltageBufType);
 
-  CHECK_LERP_CELL_VOLTAGE(EXTRAPOLATION_BELOW, -2.00f,
-                          ERROR_FLOATING_POINT_TYPE);
-  CHECK_LERP_CELL_VOLTAGE(EXTRAPOLATION_BELOW, -1.00f,
-                          ERROR_FLOATING_POINT_TYPE);
-
-  for (std::size_t x = 0; x < 2; ++x)
-    CHECK_LERP_CELL_VOLTAGE(NONE, +0.00f, +1.00f);
-
-  CHECK_LERP_CELL_VOLTAGE(NONE, 0.50f, 1.50f);
-  CHECK_LERP_CELL_VOLTAGE(NONE, 0.75f, 1.75f);
-
-  for (std::size_t x = 0; x < 2; ++x)
-    CHECK_LERP_CELL_VOLTAGE(NONE, 1.00f, 2.00f);
-
-  CHECK_LERP_CELL_VOLTAGE(NONE, 1.50f, 3.00f);
-  CHECK_LERP_CELL_VOLTAGE(NONE, 1.75f, 3.50f);
-
-  for (std::size_t x = 0; x < 2; ++x)
-    CHECK_LERP_CELL_VOLTAGE(NONE, 2.00f, 4.00f);
-
-  CHECK_LERP_CELL_VOLTAGE(EXTRAPOLATION_ABOVE, 3.00f,
-                          ERROR_FLOATING_POINT_TYPE);
-  CHECK_LERP_CELL_VOLTAGE(EXTRAPOLATION_ABOVE, 4.00f,
-                          ERROR_FLOATING_POINT_TYPE);
+  for (const CheckLerpCellVoltageBufType *buf_pointer = bufs;
+       buf_pointer < bufs_end; ++buf_pointer)
+    check_lerp_cell_voltage(buf_pointer->charge_discharged_from_cell,
+                            buf_pointer->cell_voltage_source_voltage,
+                            buf_pointer->the_error, &linear_buf, &binary_buf);
 }
 
 void check_lerp_cell_voltage(
-    const FloatingPointType charge, const ErrorType the_error,
-    const FloatingPointType the_voltage,
+    const FloatingPointType charge_discharged_from_cell,
+    const FloatingPointType cell_voltage_source_voltage,
+    const ErrorType the_error,
     LerpCellVoltageLinearBufType *const linear_buf_pointer,
     LerpCellVoltageBinaryBufType *const binary_buf_pointer) {
-  BOOST_CHECK(lerp_cell_voltage_linear(linear_buf_pointer, charge) ==
-              the_voltage);
+  BOOST_CHECK(lerp_cell_voltage_linear(linear_buf_pointer,
+                                       charge_discharged_from_cell) ==
+              cell_voltage_source_voltage);
   BOOST_CHECK(error == the_error);
   error = NONE;
-  BOOST_CHECK(lerp_cell_voltage_binary(binary_buf_pointer, charge) ==
-              the_voltage);
+  BOOST_CHECK(lerp_cell_voltage_binary(binary_buf_pointer,
+                                       charge_discharged_from_cell) ==
+              cell_voltage_source_voltage);
   BOOST_CHECK(error == the_error);
   error = NONE;
 }
