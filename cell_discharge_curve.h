9c2e351ae2b3bd7049846784ab27238e59b9fdab
matthew <m5w@users.noreply.github.com>
2017-10-26 22:31:38 -0400

    Add CellDischargeCurveBuf class to hold buf for lerp_linear

--- cell_discharge_curve.h
+++ cell_discharge_curve.h
@@ -14,25 +14,15 @@
 class CellDischargeCurve {
 public:
   CellDischargeCurve(std::istream &is);
-  inline FloatingPointType get_initial_work() const;
 
 private:
-  static inline FloatingPointType point_get_work(const Point *const point);
+  friend class CellDischargeCurveBuf;
   Point read_point(std::istream &is);
   inline const Point *get_points() const;
   inline std::size_t get_number_of_points() const;
   std::vector<Point> the_get_point;
   std::vector<FloatingPointType> the_get_point_work;
 };
-
-FloatingPointType CellDischargeCurve::get_initial_work() const {
-  return the_get_point_work.front();
-}
-
-FloatingPointType
-CellDischargeCurve::point_get_work(const Point *const point) {
-  return *(point->z_pointer);
-}
 
 const Point *CellDischargeCurve::get_points() const {
   return CellDischargeCurve::the_get_point.data();
---
+++ cell_discharge_curve_buf.cc
@@ -0,0 +1,24 @@
+#include "cell_discharge_curve_buf.h"
+
+CellDischargeCurveBuf::CellDischargeCurveBuf(
+    const CellDischargeCurve &the_cell_discharge_curve)
+    : the_cell_discharge_curve(the_cell_discharge_curve),
+      buf(lerp_linear_buf(the_cell_discharge_curve.get_points(),
+                          the_cell_discharge_curve.get_number_of_points())) {}
+
+FloatingPointType CellDischargeCurveBuf::lerp_open_circuit_voltage(
+    const FloatingPointType charge_supplied) {
+  const FloatingPointType open_circuit_voltage =
+      lerp_linear(&(CellDischargeCurveBuf::buf), charge_supplied);
+
+  if (BOOST_UNLIKELY(lerp_error != NONE))
+    throw std::exception();
+
+  return open_circuit_voltage;
+}
+
+FloatingPointType CellDischargeCurveBuf::lerp_work(
+    const FloatingPointType charge_supplied) const {
+  return lerp_z_linear(&(CellDischargeCurveBuf::buf), charge_supplied,
+                       point_get_work);
+}
