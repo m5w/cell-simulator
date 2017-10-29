0766b2539c3241416d7c14413f9d4cb24e046238
matthew <m5w@users.noreply.github.com>
2017-10-29 17:21:22 -0400

    Integrate rewritten libcell

--- tests/read.cc
+++ tests/read.cc
@@ -6,34 +6,32 @@
 #include <limits>
 #include <vector>
 
-#include "cell.h"
-#include "cell_discharge_curve.h"
 #include "floating_point_type.h"
 #include "point.h"
+
+#include "cell.h"
+#include "cell_buf.h"
 
 static inline void set_precision(std::ostream &os);
 static FloatingPointType
 get_module_voltage(const FloatingPointType load_current,
-                   const FloatingPointType cell_voltage_source_voltage,
+                   const FloatingPointType cell_open_circuit_voltage,
                    const FloatingPointType cell_mean_internal_conductance);
 
 int main() {
   std::ifstream is("../test-liion-06.bin");
   is.exceptions(std::ios_base::badbit | std::ios_base::failbit |
                 std::ios_base::eofbit);
-  std::vector<FloatingPointType> cell_discharge_curve_point_work_values;
-  std::vector<Point> cell_discharge_curve_points;
-  const CellDischargeCurve cell_discharge_curve = read_cell_discharge_curve(
-      is, cell_discharge_curve_point_work_values, cell_discharge_curve_points);
+  const Cell the_cell(is);
+  FloatingPointType cell_initial_work;
   FloatingPointType cell_mean_internal_conductance;
-  const Cell cell =
-      read_cell(is, cell_discharge_curve, cell_mean_internal_conductance);
-  FloatingPointType cell_voltage_source_voltage;
-  Cell the_cell(cell, cell_voltage_source_voltage);
+  FloatingPointType cell_open_circuit_voltage;
+  CellBuf the_cell_buf = the_cell.create_cell_buf(
+      cell_initial_work, cell_mean_internal_conductance,
+      cell_open_circuit_voltage);
   constexpr FloatingPointType load_current = 5.0;
-  FloatingPointType module_voltage =
-      get_module_voltage(load_current, cell_voltage_source_voltage,
-                         cell_mean_internal_conductance);
+  FloatingPointType module_voltage = get_module_voltage(
+      load_current, cell_open_circuit_voltage, cell_mean_internal_conductance);
   FloatingPointType time = 0.0;
   constexpr FloatingPointType change_in_time = 1.0;
   std::size_t number_of_changes_in_time = 0;
@@ -43,10 +41,10 @@ int main() {
   while (module_voltage >= 2.5) {
     std::cerr << std::setw(14) << time << std::setw(0) << "  "
               << module_voltage << std::setw(0) << '\n';
-    cell_voltage_source_voltage = the_cell.get_next_voltage_source_voltage(
+    cell_open_circuit_voltage = the_cell_buf.get_next_open_circuit_voltage(
         change_in_time, module_voltage);
     module_voltage =
-        get_module_voltage(load_current, cell_voltage_source_voltage,
+        get_module_voltage(load_current, cell_open_circuit_voltage,
                            cell_mean_internal_conductance);
     load_work += load_current * module_voltage;
     ++number_of_changes_in_time;
@@ -54,10 +52,9 @@ int main() {
   }
 
   std::cerr << '\n';
-  const FloatingPointType cell_initial_work = the_cell.get_initial_work();
   set_precision(std::cout);
   std::cout << load_work / cell_initial_work << '\n';
-  std::cout << cell_initial_work - the_cell.get_work() - load_work << '\n';
+  std::cout << cell_initial_work - the_cell_buf.get_work() - load_work << '\n';
 }
 
 void set_precision(std::ostream &os) {
@@ -66,9 +63,9 @@ void set_precision(std::ostream &os) {
 
 FloatingPointType
 get_module_voltage(const FloatingPointType load_current,
-                   const FloatingPointType cell_voltage_source_voltage,
+                   const FloatingPointType cell_open_circuit_voltage,
                    const FloatingPointType cell_mean_internal_conductance) {
-  return (cell_voltage_source_voltage * cell_mean_internal_conductance -
+  return (cell_open_circuit_voltage * cell_mean_internal_conductance -
           load_current) /
          cell_mean_internal_conductance;
 }
